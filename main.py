d1518d2ff4eab1882884eaa00296a4d982a4ca9a
ירדן מזוז <yardenden123@gmail.com>
2022-12-22 22:44:27 +0200

    New Commit

--- main.py
+++ main.py
@@ -1,74 +1,41 @@
 #  written in python 3.9
 import serial.tools.list_ports
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Controller
+
+
+def button_inputs_to_keyboard_inputs():
+    current_guitar_input = 0
+    for current_keyboard_input in keyboard_inputs:
+        if guitar_inputs[current_guitar_input] == "0":
+            keyboard.press(current_keyboard_input)
+        else:
+            keyboard.release(current_keyboard_input)
+        current_guitar_input += 1
+
+
+def port_configuration():
+    ports = serial.tools.list_ports.comports()
+    port_list = []
+
+    for one_port in ports:
+        port_list.append(str(one_port))
+        print(str(port_list))
+
+    chosen_port_number = input('Which port is your guitar connected to? COM')
+    serial_inst.baudrate = 9600
+    serial_inst.port = "COM" + str(chosen_port_number)
+    serial_inst.open()
+
 
 keyboard = Controller()
-ports = serial.tools.list_ports.comports()
-serialInst = serial.Serial()
-
-portList = []
-
-for onePort in ports:
-    portList.append(str(onePort))
-    print(str(portList))  # allowing u to see which port the guitar is connected to.
-
-port = input('Which port is your guitar connected to?')  # allowing u to choose the guitar's port
-
-portVar = "COM" + str(port)
-serialInst.baudrate = 9600
-serialInst.port = portVar
-serialInst.open()
-inputs = ['a', 's', 'd', 'e', 'w', 'q', 'z', 'x', 'c']  # the game inputs
+keyboard_inputs = ['a', 's', 'd', 'e', 'w', 'q', 'z', 'x', 'c']
+serial_inst = serial.Serial()
+port_configuration()
 
 while True:
-    if serialInst.in_waiting:
-        packet = serialInst.readline()
-        packet = packet.decode('utf').rstrip('\n')  # taking button inputs from the chosen come which the guitar is
+    if serial_inst.in_waiting:
+        guitar_inputs = serial_inst.readline()  # taking button inputs from the chosen COM which the guitar is
         # connected to, the input comes in the from of 1 and 0 so that 1 is unpressed button and 0 is pressed
-        print(packet)
-        g = 0
-        for i in inputs:
-            if packet[g] == "0":
-                keyboard.press(i)
-            else:
-                keyboard.release(i)
-            g += 1
+        guitar_inputs = guitar_inputs.decode('utf').rstrip('\n')
 
-        # __old code__
-
-        # if packet[0] == "0":
-        #     keyboard.press('a')
-        # else:
-        #     keyboard.release('a')
-        # if packet[1] == "0":
-        #     keyboard.press('s')
-        # else:
-        #     keyboard.release('s')
-        # if packet[2] == "0":
-        #     keyboard.press('d')
-        # else:
-        #     keyboard.release('d')
-        # if packet[3] == "0":
-        #     keyboard.press('e')
-        # else:
-        #     keyboard.release('e')
-        # if packet[4] == "0":
-        #     keyboard.press('w')
-        # else:
-        #     keyboard.release('w')
-        # if packet[5] == "0":
-        #     keyboard.press('q')
-        # else:
-        #     keyboard.release('q')
-        # if packet[6] == "0":
-        #     keyboard.press('z')
-        # else:
-        #     keyboard.release('z')
-        # if packet[7] == "0":
-        #     keyboard.press('x')
-        # else:
-        #     keyboard.release('x')
-        # if packet[8] == "0":
-        #     keyboard.press('c')
-        # else:
-        #     keyboard.release('c')
+        button_inputs_to_keyboard_inputs()
